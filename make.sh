a71c9da0b765c4836890264f64907d2b99937093
SamsonWang <wangzhilv@gmail.com>
2018-12-14 23:35:46 +0800

    update make script

--- make.sh
+++ make.sh
@@ -2,16 +2,21 @@
 
 SOURCEDIR="source/$1"
 BUILDDIR="docs/$1"
-LANG=$1
+PARAM1=$1
 
 function judge_lang () {
-    case $LANG in
+    case $PARAM1 in
+        index)
+            echo "Copy index"
+            copy_index ;;
         en)
-            echo "Building English Vesrion" ;;
+            echo "Building English Vesrion"
+            build ;;
         zhCN)
-            echo "Building Chinese Version" ;;
+            echo "Building Chinese Version"
+            build ;;
         * )
-            echo "Invalid Language: $LANG"
+            echo "Invalid param1: $PARAM1"
             exit ;;
     esac
 }
@@ -20,8 +25,12 @@ function build() {
     sphinx-build -a -E -j auto -b html ${SOURCEDIR} ${BUILDDIR}
 }
 
+function copy_index() {
+    cp -f source/index.html docs
+    cp -f source/.nojekyll docs
+    cp -f source/CNAME docs
+}
 
 if [ $# -gt 0 ]; then
     judge_lang
-    build
 fi
